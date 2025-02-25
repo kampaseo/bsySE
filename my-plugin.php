60e14255a5b228da968f35e2de8dfdb26154e530
kampaseo <seo@kampa.com.tr>
2025-02-25 09:56:46 +0300

    Create my-plugin.php

---
+++ my-plugin.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: My Custom Plugin
+Plugin URI: https://example.com/my-plugin
+Description: Bu, özel bir WordPress eklentisidir.
+Version: 1.0
+Author: Your Name
+Author URI: https://example.com
+License: GPL2
+*/
